f1a204f8fafa4a8df86c6bbc7fd959a6284e6771
Juan Dorado <jfdoradotr@icloud.com>
2024-12-12 12:00:56 -0600

    Create the form UI

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -5,8 +5,26 @@
 import SwiftUI
 
 struct AddView: View {
+  @State private var name = ""
+  @State private var type = "Personal"
+  @State private var amount = 0.0
+
+  let types = ["Business", "Personal"]
+
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    NavigationStack {
+      Form {
+        TextField("Name", text: $name)
+        Picker("Type", selection: $type) {
+          ForEach(types, id: \.self) {
+            Text($0)
+          }
+        }
+        TextField("Amount", value: $amount, format: .currency(code: "USD"))
+          .keyboardType(.decimalPad)
+      }
+      .navigationTitle("Add New Expense")
+    }
   }
 }
 
