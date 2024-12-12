a9e425f4f88b1821fff56d8e7055dc64772b29e7
Juan Dorado <jfdoradotr@icloud.com>
2024-12-12 12:10:40 -0600

    Load the saved items in case they exist on UserDefaults

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -20,6 +20,16 @@ class Expenses {
       }
     }
   }
+
+  init() {
+    if let savedItems = UserDefaults.standard.data(forKey: "Items") {
+      if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+        items = decodedItems
+        return
+      }
+    }
+    items = []
+  }
 }
 
 struct ContentView: View {
