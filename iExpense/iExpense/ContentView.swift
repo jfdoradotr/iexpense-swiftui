91346374ea7e7873eeac13ae3960c8a8f9f548be
Juan Dorado <jfdoradotr@icloud.com>
2024-12-12 12:14:39 -0600

    Show more information on the row

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -40,7 +40,16 @@ struct ContentView: View {
     NavigationStack {
       List {
         ForEach(expenses.items) { item in
-          Text(item.name)
+          HStack {
+            VStack(alignment: .leading) {
+              Text(item.name)
+                .font(.headline)
+              Text(item.type)
+                .font(.subheadline)
+            }
+            Spacer()
+            Text(item.amount, format: .currency(code: "USD"))
+          }
         }
         .onDelete(perform: removeItems)
       }
