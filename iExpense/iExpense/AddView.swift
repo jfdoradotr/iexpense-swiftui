d7fbb3a1bf6982f42d6aaa8ce4601d31f80d817d
Juan Dorado <jfdoradotr@icloud.com>
2024-12-17 09:28:28 -0600

    Move the name for the new expense as a part of the navigation title to edit in place

--- iExpense/iExpense/AddView.swift
+++ iExpense/iExpense/AddView.swift
@@ -7,7 +7,7 @@ import SwiftUI
 struct AddView: View {
   @Environment(\.dismiss) var dismiss
 
-  @State private var name = ""
+  @State private var name = "New Expense"
   @State private var type = "Personal"
   @State private var amount = 0.0
 
@@ -18,7 +18,6 @@ struct AddView: View {
   var body: some View {
     NavigationStack {
       Form {
-        TextField("Name", text: $name)
         Picker("Type", selection: $type) {
           ForEach(types, id: \.self) {
             Text($0)
@@ -27,7 +26,7 @@ struct AddView: View {
         TextField("Amount", value: $amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
           .keyboardType(.decimalPad)
       }
-      .navigationTitle("Add New Expense")
+      .navigationTitle($name)
       .navigationBarTitleDisplayMode(.inline)
       .toolbar {
         Button("Save") {
