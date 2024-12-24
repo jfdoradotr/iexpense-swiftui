ccf359622537bca630cd95f8dfea7301087248d2
Juan Dorado <jfdoradotr@icloud.com>
2024-12-23 18:04:45 -0600

    Move the Expenses Store into its own file

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -4,35 +4,6 @@
 
 import SwiftUI
 
-@Observable
-class Expenses {
-  var items = [ExpenseItem]() {
-    didSet {
-      if let encoded = try? JSONEncoder().encode(items) {
-        UserDefaults.standard.set(encoded, forKey: "Items")
-      }
-    }
-  }
-
-  var personalItems: [ExpenseItem] {
-    items.filter { $0.type == "Personal" }
-  }
-
-  var businessItems: [ExpenseItem] {
-    items.filter { $0.type == "Business" }
-  }
-
-  init() {
-    if let savedItems = UserDefaults.standard.data(forKey: "Items") {
-      if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
-        items = decodedItems
-        return
-      }
-    }
-    items = []
-  }
-}
-
 struct ContentView: View {
   @State private var expenses = Expenses()
 
