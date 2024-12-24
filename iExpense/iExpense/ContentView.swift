bb0f7dca9e03804926b753b8b063282f21f316cb
Juan Dorado <jfdoradotr@icloud.com>
2024-12-24 11:55:02 -0600

    Add sorting, moving the content into a new view to inject the sorting

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -6,78 +6,82 @@ import SwiftData
 import SwiftUI
 
 struct ContentView: View {
-  @Environment(\.modelContext) var modelContext
-  @Query private var items: [Expense]
+  @State private var sortOrder = [
+    SortDescriptor(\Expense.name),
+    SortDescriptor(\Expense.amount)
+  ]
 
   var body: some View {
     NavigationStack {
-      List {
-        Section {
-          ForEach(items) { item in
-            HStack {
-              VStack(alignment: .leading, spacing: 5) {
-                Text(item.name)
-                  .font(.headline)
-                HStack(spacing: 5) {
-                  Image(systemName: item.typeImageName)
-                  Text(item.type)
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-              }
-              Spacer()
-              Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                .font(item.amount < 10 ? .footnote : item.amount < 100 ? .subheadline : .headline)
-                .foregroundStyle(item.amount < 10 ? .green : item.amount < 100 ? .yellow : .red)
+      ExpensesView(sortOrder: sortOrder)
+        .navigationTitle("iExpense")
+        .toolbar {
+          Menu {
+            Picker("Sort", selection: $sortOrder) {
+              Text("Sort by Name")
+                .tag([
+                  SortDescriptor(\Expense.name),
+                  SortDescriptor(\Expense.amount)
+                ])
+              Text("Sort by Amount")
+                .tag([
+                  SortDescriptor(\Expense.amount),
+                  SortDescriptor(\Expense.name)
+                ])
             }
+          } label: {
+            Label("Sort", systemImage: "arrow.up.arrow.down")
           }
-          .onDelete(perform: removeExpenses)
-        }
-      }
-      .navigationTitle("iExpense")
-      .toolbar {
-        NavigationLink {
-          AddView()
-        } label: {
+          NavigationLink {
+            AddView()
+          } label: {
             Image(systemName: "plus")
+          }
         }
-      }
     }
   }
 
-  func removeExpenses(at offsets: IndexSet) {
-    for offset in offsets {
-      let item = items[offset]
-      modelContext.delete(item)
-    }
-  }
+
 }
 
 // MARK: - SectionView
 
 private extension ContentView {
-  struct SectionView: View {
-    let name: String
-    let items: [Expense]
-    let onDelete: (IndexSet) -> Void
+  struct ExpensesView: View {
+    @Environment(\.modelContext) var modelContext
+
+    @Query var expenses: [Expense]
 
     var body: some View {
-      Section(header: Text(name)) {
-        ForEach(items) { item in
-          HStack {
-            VStack(alignment: .leading) {
-              Text(item.name)
-                .font(.headline)
-              Image(systemName: item.typeImageName)
-                .font(.subheadline)
+      List {
+        Section {
+          ForEach(expenses) { expense in
+            HStack {
+              VStack(alignment: .leading) {
+                Text(expense.name)
+                  .font(.headline)
+                Image(systemName: expense.typeImageName)
+                  .font(.subheadline)
+              }
+              Spacer()
+              Text(expense.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                .font(expense.amount < 10 ? .footnote : expense.amount < 100 ? .subheadline : .headline)
+                .foregroundStyle(expense.amount < 10 ? .green : expense.amount < 100 ? .yellow : .red)
             }
-            Spacer()
-            Text(item.amount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-              .font(item.amount < 10 ? .footnote : item.amount < 100 ? .subheadline : .headline)
-              .foregroundStyle(item.amount < 10 ? .green : item.amount < 100 ? .yellow : .red)
           }
+          .onDelete(perform: removeExpenses)
         }
-        .onDelete(perform: onDelete)
+      }
+    }
+
+    init(sortOrder: [SortDescriptor<Expense>]) {
+      _expenses = Query(sort: sortOrder)
+    }
+
+    private func removeExpenses(at offsets: IndexSet) {
+      for offset in offsets {
+        let expense = expenses[offset]
+        modelContext.delete(expense)
       }
     }
   }
